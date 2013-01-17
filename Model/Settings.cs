562ba66fbf38427bd10c85acd64a6cc448a34eea
Doeke Zanstra <doeke@zanstra.net>
2013-01-17 14:48:55 +0100

    Moved Settings to Model.

---
+++ Model/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace Dinomopabot.Model
+{
+    using System;
+    using System.IO;
+
+    class Settings
+    {
+        public Settings()
+        {
+            Path = Environment.CurrentDirectory;
+            Subdirectories = SearchOption.AllDirectories;
+            Filter = "*.*";
+            All = false;
+        }
+
+        public string Path { get; set; }
+        public SearchOption Subdirectories { get; set; }
+        public string Filter { get; set; }
+        public bool All { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,22 +8,6 @@
     using Dinomopabot.Utils;
     using Mono.Options;
 
-    class Settings
-    {
-        public Settings()
-        {
-            Path = Environment.CurrentDirectory;
-            Subdirectories = SearchOption.AllDirectories ;
-            Filter = "*.*";
-            All = false;
-        }
-
-        public string Path { get; set; }
-        public SearchOption Subdirectories { get; set; }
-        public string Filter { get; set; }
-        public bool All { get; set; }
-    }
-
     class Program
     {
         static int Main(string[] args)
